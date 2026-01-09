62f1a096f49866ddc30a854b8e64b7e291b36c39
Tanathip Pai <tanathip.jai@gmail.com>
2026-01-09 10:58:26 +0000

    add new function

--- hello.py
+++ hello.py
@@ -2,3 +2,10 @@
 print("hello world")
 print("I am using github with posit.cloud")
 print("Hello")
+
+def hello():
+  name = input("What's your name:")
+  print(f"Hello {name}!")
+  
+# test function
+hello()
